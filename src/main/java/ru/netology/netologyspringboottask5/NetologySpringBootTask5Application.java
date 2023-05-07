a18dd1c7fc063a6c29eb10f11714e8f9b1123479
Vyacheslav Nechepurenko <masterdog8@mail.ru>
2023-05-07 13:37:09 +0300

    initial commit

---
+++ src/main/java/ru/netology/netologyspringboottask5/NetologySpringBootTask5Application.java
@@ -0,0 +1,13 @@
+package ru.netology.netologyspringboottask5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetologySpringBootTask5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetologySpringBootTask5Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/netology/netologyspringboottask5/NetologySpringBootTask5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.netologyspringboottask5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetologySpringBootTask5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
